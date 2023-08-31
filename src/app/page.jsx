2a9699fb5a4495a52b2454cb52ef461c42433d43
Gautam Jain <astrogeek77@gmail.com>
2023-08-31 16:19:05 +0530

    removed serverprops function

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -428,19 +428,19 @@ export default function Home() {
   )
 }
 
-export async function getServerSideProps({ req }) {
-  const session = await getSession({ req })
+// export async function getServerSideProps({ req }) {
+//   const session = await getSession({ req })
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    }
-  }
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/login',
+//         permanent: false,
+//       },
+//     }
+//   }
 
-  return {
-    props: { session },
-  }
-}
+//   return {
+//     props: { session },
+//   }
+// }
